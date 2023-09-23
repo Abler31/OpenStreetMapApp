d0c3309b5980c8f4bbf838e3bb82f828bfb5a16f
Abler31 <58187511+Abler31@users.noreply.github.com>
2023-09-23 18:28:35 +0300

    map visibility

--- app/src/main/java/com/abler31/mapapp/MainActivity.kt
+++ app/src/main/java/com/abler31/mapapp/MainActivity.kt
@@ -1,13 +1,107 @@
 package com.abler31.mapapp
 
+import android.graphics.Rect
+import android.location.GpsStatus
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import org.osmdroid.api.IMapController
+import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapListener
+import org.osmdroid.events.ScrollEvent
+import org.osmdroid.events.ZoomEvent
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    lateinit var mMap: MapView
+    lateinit var controller: IMapController
+    lateinit var mMyLocationOverlay: MyLocationNewOverlay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
+
+        mMap = findViewById(R.id.osmmap)
+        mMap.setTileSource(TileSourceFactory.MAPNIK)
+        mMap.mapCenter
+
+        mMap.setMultiTouchControls(true)
+        mMap.getLocalVisibleRect(Rect())
+
+
+        //mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
+        controller = mMap.controller
+
+        /*mMyLocationOverlay.enableMyLocation()
+        mMyLocationOverlay.enableFollowLocation()
+        mMyLocationOverlay.isDrawAccuracyEnabled = true
+        mMyLocationOverlay.runOnFirstFix {
+            runOnUiThread {
+                controller.setCenter(mMyLocationOverlay.myLocation);
+                controller.animateTo(mMyLocationOverlay.myLocation)
+            }
+        }
+*/
+        val mapPoint = GeoPoint(55.7833, 49.1017)
+
+        controller.setZoom(6.0)
+
+        Log.e("TAG", "onCreate:in ${controller.zoomIn()}")
+        Log.e("TAG", "onCreate: out  ${controller.zoomOut()}")
+
+        controller.animateTo(mapPoint)
+        //mMap.overlays.add(mMyLocationOverlay)
+
+
+
     }
+
+    /*override fun onScroll(event: ScrollEvent?): Boolean {
+        // event?.source?.getMapCenter()
+        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
+        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
+        //  Log.e("TAG", "onScroll   x: ${event?.x}  y: ${event?.y}", )
+        return true
+    }
+
+    override fun onZoom(event: ZoomEvent?): Boolean {
+        //  event?.zoomLevel?.let { controller.setZoom(it) }
+
+
+        Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
+        return false;
+    }
+
+    override fun onGpsStatusChanged(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionsToRequest = ArrayList<String>()
+        var i = 0
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i])
+            i++
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+    }*/
 }
