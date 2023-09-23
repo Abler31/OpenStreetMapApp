7257a469a76d86652aed509e83b9a86f0cfe7c64
Abler31 <58187511+Abler31@users.noreply.github.com>
2023-09-23 22:00:28 +0300

    map control buttons added

--- app/src/main/java/com/abler31/mapapp/MainActivity.kt
+++ app/src/main/java/com/abler31/mapapp/MainActivity.kt
@@ -20,10 +20,8 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     lateinit var mMap: MapView
     lateinit var controller: IMapController
-    lateinit var mMyLocationOverlay: MyLocationNewOverlay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,75 +31,16 @@ class MainActivity : AppCompatActivity() {
         mMap = findViewById(R.id.osmmap)
         mMap.setTileSource(TileSourceFactory.MAPNIK)
         mMap.mapCenter
-
         mMap.setMultiTouchControls(true)
         mMap.getLocalVisibleRect(Rect())
 
-
-        //mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
         controller = mMap.controller
 
-        /*mMyLocationOverlay.enableMyLocation()
-        mMyLocationOverlay.enableFollowLocation()
-        mMyLocationOverlay.isDrawAccuracyEnabled = true
-        mMyLocationOverlay.runOnFirstFix {
-            runOnUiThread {
-                controller.setCenter(mMyLocationOverlay.myLocation);
-                controller.animateTo(mMyLocationOverlay.myLocation)
-            }
-        }
-*/
         val mapPoint = GeoPoint(55.7833, 49.1017)
 
-        controller.setZoom(6.0)
+        controller.setZoom(15.5)
 
-        Log.e("TAG", "onCreate:in ${controller.zoomIn()}")
-        Log.e("TAG", "onCreate: out  ${controller.zoomOut()}")
 
         controller.animateTo(mapPoint)
-        //mMap.overlays.add(mMyLocationOverlay)
-
-
-
     }
-
-    /*override fun onScroll(event: ScrollEvent?): Boolean {
-        // event?.source?.getMapCenter()
-        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
-        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
-        //  Log.e("TAG", "onScroll   x: ${event?.x}  y: ${event?.y}", )
-        return true
-    }
-
-    override fun onZoom(event: ZoomEvent?): Boolean {
-        //  event?.zoomLevel?.let { controller.setZoom(it) }
-
-
-        Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel}   source:  ${event?.source}")
-        return false;
-    }
-
-    override fun onGpsStatusChanged(p0: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        val permissionsToRequest = ArrayList<String>()
-        var i = 0
-        while (i < grantResults.size) {
-            permissionsToRequest.add(permissions[i])
-            i++
-        }
-        if (permissionsToRequest.size > 0) {
-            ActivityCompat.requestPermissions(
-                this,
-                permissionsToRequest.toTypedArray(),
-                REQUEST_PERMISSIONS_REQUEST_CODE)
-        }
-    }*/
 }
